d518e24bcf8d501b7817af4a7e4c3624a2a5376a
Margheriti <lilrom13@gmail.com>
2016-12-14 11:52:22 -0500

    change db connection

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var port  	 = 3000;
 var cors = require('cors');
 
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<heroku_9rp7v59d>:<totodu13>@ds133358.mlab.com:33358/heroku_9rp7v59d');
+var uristring = process.env.MONGOLAB_URI
+
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
 
 var part = require('./routes/part');
 
